d209b4c8c9a01b94922d0b0d5638e3d113a1de80
ASHWANI TRIPATHI <ashwani.tripathi.505850@gmail.com>
2026-06-19 20:36:08 +0530

    Create Implement_Queue_using_Stacks.cpp

---
+++ Week_3/Day_5/Implement_Queue_using_Stacks.cpp
@@ -0,0 +1,39 @@
+#include <queue>
+using namespace std;
+
+class MyQueue {
+private:
+    queue<int> q;
+
+public:
+    MyQueue() {
+        // Constructor
+    }
+
+    void push(int x) {
+        q.push(x);
+    }
+
+    int pop() {
+        int val = q.front();
+        q.pop();
+        return val;
+    }
+
+    int peek() {
+        return q.front();
+    }
+
+    bool empty() {
+        return q.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
