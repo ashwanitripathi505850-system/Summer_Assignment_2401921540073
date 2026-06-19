d8cd590aebd0bcfbdb2bc89e9fa2a18eee77b227
ASHWANI TRIPATHI <ashwani.tripathi.505850@gmail.com>
2026-06-19 20:50:35 +0530

    Create Sliding_Window_Maximum.cpp

---
+++ Week_3/Day_5/Sliding_Window_Maximum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+         vector<int> res;
+        deque<int> dq;
+        for (int i=0;i<k;++i){
+            while (!dq.empty() && nums[i] >= nums[dq.back()])
+                dq.pop_back();
+            dq.push_back(i);
+        }
+        for (int i = k; i < nums.size(); ++i) {
+            res.push_back(nums[dq.front()]);
+            while (!dq.empty() && dq.front() <= i - k)
+                dq.pop_front();
+            while (!dq.empty() && nums[i] >= nums[dq.back()])
+                dq.pop_back();
+
+            dq.push_back(i);
+        }
+
+        res.push_back(nums[dq.front()]);
+        return res;
+        
+    }
+};
